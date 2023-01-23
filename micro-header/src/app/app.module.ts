592774dc2bc27b8ef9aa1b30d2c6a12057544876
Md.Moniruzzaman <moni.return@gmail.com>
2023-01-23 16:02:40 +0600

    update header

--- micro-header/src/app/app.module.ts
+++ micro-header/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialExampleModule } from 'src/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { SubtitleComponent } from './subtitle/subtitle.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialExampleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
